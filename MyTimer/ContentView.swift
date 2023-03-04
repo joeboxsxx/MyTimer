d58472977e3dd69f8b2bc1a1f38cbe6e1d1e16cc
joeboxsxx <joeboxsxx@ezweb.ne.jp>
2023-03-05 02:27:27 +0900

    Initial Commit

---
+++ MyTimer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyTimer
+//
+//  Created by 戸上健太 on 2023/03/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by 戸上健太 on 2023/03/05.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
